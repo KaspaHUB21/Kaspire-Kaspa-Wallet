82b0225550127087a4cc382c12322594a2f8e741
KaspaHUB21 <hub21@users.noreply.github.com>
2026-07-29 22:46:46 +0200

    chore: publish Kaspire 0.11.4 website

--- website/app/release.ts
+++ website/app/release.ts
@@ -1,5 +1,5 @@
 export const currentRelease = {
-  version: "0.11.1",
-  build: 59,
-  sha256: "ab61b60f30c8527a8a81858b4f8c611862e39eed20b664fa86f36ecffd424346",
+  version: "0.11.4",
+  build: 62,
+  sha256: "341e1920a585b279a87e4a1032d12b9442d1563b0239d07ba4cffd695e610610",
 } as const;
